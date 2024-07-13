0154389127236159cabb4687a2fd57cca6e5a531
fadil <fadhilshareef369@gmail.com>
2024-07-14 00:05:47 +0530

    api

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodroute.js';
+
+
+
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+//api endpoint
+app.use("/api/food",foodRouter);
+app.use("/images",express.static("uploads"));
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send('server is ready');
+})
+
+app.listen(3000,()=>{
+    console.log('server started at 3000');
+})
+
+//mongodb+srv://admin-fadil:<password>@cluster0.u3bs9zf.mongodb.net/?
